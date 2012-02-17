3021657c1d563fc870cc63cf1f1f5be2e32130af
aeoth <paul@theleagueofpaul.com>
2012-02-17 22:52:33 +1100

    some slow and naive filtering

--- src/MilestoneForWindows/Views/OverviewView.xaml.cs
+++ src/MilestoneForWindows/Views/OverviewView.xaml.cs
@@ -1,11 +1,63 @@
-﻿namespace MilestoneForWindows.Views
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using MilestoneForWindows.ViewModels;
+
+namespace MilestoneForWindows.Views
 {
     public partial class OverviewView
     {
+        private CollectionViewSource _cvsPullRequests;
+        private CollectionViewSource _cvs;
         public OverviewView()
         {
             InitializeComponent();
+            Loaded += OverviewViewLoaded;
         }
 
+        void OverviewViewLoaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _cvsPullRequests = Resources["cvsPullRequests"] as CollectionViewSource;
+            _cvs = Resources["cvs"] as CollectionViewSource;
+            _cvs.Filter += CvsFilter;
+            _cvsPullRequests.Filter += CvsPullRequestsFilter;
+        }
+
+        private void TxtFilter_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            _cvs.View.Refresh();
+            _cvsPullRequests.View.Refresh();
+        }
+
+        void CvsFilter(object sender, FilterEventArgs e)
+        {
+            var item = ((IssueViewModel)e.Item);
+            e.Accepted = false;
+            if (item.Issue.User.Login.Contains(txtFilter.Text))
+            {
+                e.Accepted = true;
+                return;
+            }
+            else if (item.Issue.Title.Contains(txtFilter.Text))
+            {
+                e.Accepted = true;
+                return;
+            }
+        }
+
+        void CvsPullRequestsFilter(object sender, FilterEventArgs e)
+        {
+            var item = ((PullRequestViewModel) e.Item);
+            e.Accepted = false;
+            if (item.Pull.User.Login.Contains(txtFilter.Text))
+            {
+                e.Accepted = true;
+                return;
+            }
+            else if (item.Pull.Title.Contains(txtFilter.Text))
+            {
+                e.Accepted = true;
+                return;
+            }
+        }
     }
 }
